88b2d0e2a1e73b9a01a80542530921079acd0b06
Jorge Gomes <jorge.gomes@vost.pt>
2022-11-07 06:52:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -153,4 +153,4 @@ def whatistoday(n_intervals):
 # -------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-	app.run_server(host='0.0.0.0', debug=True)
+	app.run_server(host='0.0.0.0', debug=False)
